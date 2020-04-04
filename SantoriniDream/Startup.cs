8d9f5f78065f4aa2f8f1bc67a58d428a1d2651d0
hygoh <hygoh2k@gmail.com>
2020-04-04 21:31:36 +0800

    init project

---
+++ SantoriniDream/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SantoriniDream.Startup))]
+namespace SantoriniDream
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
